f323158286391db3a2123ef87e4e52cde601ba2a
Francisco Sousa <115943937+xicobski@users.noreply.github.com>
2023-08-27 14:42:00 +0100

    update

--- lib/info/workshop.dart
+++ lib/info/workshop.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:js_interop';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
