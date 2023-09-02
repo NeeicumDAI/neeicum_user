7289432501bd3dfb9193dc7473e7c0f77337e71f
RuiRocha23 <96590570+RuiRocha23@users.noreply.github.com>
2023-09-02 16:29:37 +0100

    nots

--- lib/firebaseapi.dart
+++ lib/firebaseapi.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:async';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> handleBackgroundMessage(RemoteMessage message) async {
   print('Title: ${message.notification?.title}');
@@ -9,11 +13,24 @@ Future<void> handleBackgroundMessage(RemoteMessage message) async {
 
 class FirebaseApi {
   final _firebaseMessaging = FirebaseMessaging.instance;
+  String? uid = FirebaseAuth.instance.currentUser?.uid.trim();
 
   Future<void> initNotifications() async {
     await _firebaseMessaging.requestPermission();
     final fCMToken = await _firebaseMessaging.getToken();
     print('Token: $fCMToken');
+    setToken(fCMToken);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  }
+
+  void setToken(final fCMToken) async {
+    String? uid = FirebaseAuth.instance.currentUser?.uid.trim();
+    final ref =
+        FirebaseDatabase.instance.ref().child('users').child(uid.toString());
+
+    final snap = await ref.get();
+    if (!(snap.exists)) {
+      ref.update({'token': fCMToken.toString()});
+    }
   }
 }
